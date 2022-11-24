bc814a2dc8aede5c7e87b78675f1bef447895659
rldiaz14 <rldiaz14.1@gmail.com>
2022-11-23 22:13:39 -0500

    Initial commit

---
+++ src/myfirst/MyFirst.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package myfirst;
+
+/**
+ *
+ * @author Ramon
+ */
+public class MyFirst {
+
+    /**
+     * @param args the command line arguments
+     * Greek Alphabet oe
+     */
+    public static void main(String[] args) {
+       for (char  c = 0x0370; c <= 0x03FF; c++)
+        System.out.print(c+" ");
+    }
+    
+}
